85a9c05f84ad576b0f417d935a689e4efc5824f7
mr.peng <mypzj@foxmail.com>
2019-06-02 13:31:12 +0800

    修改生成规则

--- FreeSql.Tools/Models/RazorModel.cs
+++ FreeSql.Tools/Models/RazorModel.cs
@@ -7,19 +7,39 @@ using System.Threading.Tasks;
 
 namespace FreeSqlTools.Models
 {
-  public  class RazorModel
+    public class RazorModel
     {
         public IFreeSql fsql { get; set; }
-        public List<DbTableInfo> Tables { get; set; }
-        public DbTableInfo Table { get; set; }
-        public string TableName { get; set; }
-        public Func<string, string> GetEntityName { get; set; }
-        public Func<string, string> GetPropertyName { get; set; }
-        public RazorModel(IFreeSql freeSql)
-        {
-            this.fsql = freeSql;
-            this.Tables = freeSql.DbFirst.GetTablesByDatabase();            
+        public List<DbColumnInfo> Columns { get; set; }
+        public DbTableInfo table { get; set; }
+        public Func<Models.TaskBuild, string> GetEntityName { get; set; }
+        public string Namespace { get; set; }
 
-        }
+
+        public string FullTableName => $"{table.Schema}.{table.Name}".TrimStart('.');
+
+
+
+        public Func<DbColumnInfo, GetDbInfoModel> GetDbInfo => col => {
+            var info = fsql.CodeFirst.GetDbInfo(col.CsType);
+            if (info == null) return null;
+            return new GetDbInfoModel
+            {
+                type = info.Value.type,
+                dbtype = info.Value.dbtype,
+                dbtypeFull = info.Value.dbtypeFull.Replace(" NOT NULL", ""),
+                isnullable = info.Value.isnullable,
+                defaultValue = info.Value.defaultValue
+            };
+        };
+    }
+
+    public class GetDbInfoModel
+    {
+        public int type { get; set; }
+        public string dbtype { get; set; }
+        public string dbtypeFull { get; set; }
+        public bool? isnullable { get; set; }
+        public object defaultValue { get; set; }
     }
 }
