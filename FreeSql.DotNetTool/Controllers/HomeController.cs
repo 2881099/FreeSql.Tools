7493775967f1763affb5216ab6b66b10f13ff2b2
28810 <28810@YEXIANGQIN>
2019-10-21 21:59:09 +0800

    dotnet tool

---
+++ FreeSql.DotNetTool/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeSql.Tools.Controllers
+{
+    public class HomeController : Controller
+    {
+    }
+}
