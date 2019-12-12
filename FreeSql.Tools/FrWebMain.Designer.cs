753d5785ba9e115c49295b5901881207aa63ea85
28810 <28810@YEXIANGQIN>
2019-12-12 18:47:31 +0800

    更新 nuget

--- FreeSql.Tools/FrWebMain.Designer.cs
+++ FreeSql.Tools/FrWebMain.Designer.cs
@@ -31,7 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "FreeSql.Tools 生成工具";
         }
 
         #endregion
